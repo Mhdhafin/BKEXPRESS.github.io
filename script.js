7f1d65b2a930ff6bb99417a4c030bf610855fee4
Mhdhafin <119399774+Mhdhafin@users.noreply.github.com>
2024-02-14 14:53:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toogle class active untuk search form
+const SearchForm = document.querySelector(".search-form");
+const SearchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  SearchForm.classList.toggle("active");
+  SearchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar sidebar
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !SearchForm.contains(e.target)) {
+    SearchForm.classList.remove("active");
   }
 });
